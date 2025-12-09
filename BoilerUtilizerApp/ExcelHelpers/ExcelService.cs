69e9f710706cdbb052530784f11373c456b95147
Daniyal <daniiltemirkhanov@gmail.com>
2025-12-09 23:22:05 +0500

    ExcelService, xlsx file - basic logic

---
+++ BoilerUtilizerApp/ExcelHelpers/ExcelService.cs
@@ -0,0 +1,35 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoilerUtilizerApp.ExcelHelpers
+{
+    public class ExcelService
+    {
+        private readonly string _path;
+       
+        public ExcelService(string path)
+        {
+            _path = path;
+        }
+
+        public void WriteInput(double gasTemperature)
+        {
+            using var workbook = new XLWorkbook(_path);
+            var sheet = workbook.Worksheet("Таб1");
+            sheet.Cell("С5");
+            workbook.Save();
+        }
+        public double ReadOutput()
+        {
+            using var workbook = new XLWorkbook(_path);
+            var sheet = workbook.Worksheet("Результаты");
+            var cell = sheet.Cell("C27");
+            return double.TryParse(cell.Value.ToString(), out var val) ? val : 0;
+
+        }
+    }
+}
--- BoilerUtilizerApp/Form1.cs
+++ BoilerUtilizerApp/Form1.cs
@@ -1,10 +1,23 @@
+using BoilerUtilizerApp.ExcelHelpers;
+
 namespace BoilerUtilizerApp
 {
     public partial class Form1 : Form
     {
+        private ExcelService excel;
         public Form1()
         {
             InitializeComponent();
+            excel = new ExcelService("BoilerUtilizer.xlsx");
+              this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ???? ??? ???????
+            excel.WriteInput(180.5);                // ?????????? ??????????? ?????
+            double t = excel.ReadOutput();          // ?????? ?????????
+            MessageBox.Show($"??????????? ???? 1: {t} °C");
         }
     }
 }
